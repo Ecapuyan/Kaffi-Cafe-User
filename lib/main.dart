af1895b660981902a92cf5cf4b50eb132e2f4945
Ecapuyan <capuyan.e.bsinfotech@gmail.com>
2026-02-10 19:36:43 +0800

    feat: implement .env for firebase credentials

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,26 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:get/get_navigation/src/root/get_material_app.dart';
 import 'package:get_storage/get_storage.dart';
-import 'package:kaffi_cafe/firebase_options.dart';
-import 'package:kaffi_cafe/screens/auth/login_screen.dart';
-import 'package:kaffi_cafe/screens/home_screen.dart';
 import 'package:kaffi_cafe/widgets/auth_wrapper.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await dotenv.load(fileName: ".env");
   await Firebase.initializeApp(
     name: 'kaffi-cafe',
-    options: DefaultFirebaseOptions.currentPlatform,
+    options: FirebaseOptions(
+      apiKey: dotenv.env['FIREBASE_API_KEY']!,
+      appId: dotenv.env['FIREBASE_APP_ID']!,
+      messagingSenderId: dotenv.env['FIREBASE_MESSAGING_SENDER_ID']!,
+      projectId: dotenv.env['FIREBASE_PROJECT_ID']!,
+      authDomain: dotenv.env['FIREBASE_AUTH_DOMAIN']!,
+      storageBucket: dotenv.env['FIREBASE_STORAGE_BUCKET']!,
+    ),
   );
   await GetStorage.init();
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
